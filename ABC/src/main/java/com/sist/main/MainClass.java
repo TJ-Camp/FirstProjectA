f362b5379ba3c58d28217764406241c89a8b1dc3
sist115_1 <sist115_1@sist115>
2024-07-26 16:25:30 +0900

    1234

--- ABC/src/main/java/com/sist/main/MainClass.java
+++ ABC/src/main/java/com/sist/main/MainClass.java
@@ -4,5 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("sss");
 		System.out.println("sss");
+		System.out.println("정유엽♥심은우");
+		System.out.println("오오오오오오오오오오오오");
 	}
 }
